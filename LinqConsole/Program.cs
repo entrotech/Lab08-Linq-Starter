433421f88b526787a07889d59a655faf54f7f16e
John Darragh <darragh@entrotech.net>
2016-12-14 09:34:30 -0800

    Add project files.

---
+++ LinqConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Talent.Domain;
+using Talent.Domain.TestData;
+using UclaExt.Common;
+using UclaExt.Common.Extensions;
+
+namespace LinqConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LINQ Lab:");
+            DomainObjectStore store = new DomainObjectStore();
+            store.People.PrintToConsole();
+
+
+
+            Console.WriteLine("Press <Enter> to quit the application");
+            Console.ReadLine();
+        }
+
+   
+    }
+}
